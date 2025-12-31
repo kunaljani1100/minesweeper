5e1d8191ff3f9cfce5db3d03d836d4ea3e9105df
kunaljani1100 <168258319+kunaljani1100@users.noreply.github.com>
2025-12-31 16:36:51 -0700

    Separate function to set surrounding mines.

--- src/main/java/org/gaming/GameOperations.java
+++ src/main/java/org/gaming/GameOperations.java
@@ -1,5 +1,8 @@
 package org.gaming;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class GameOperations {
 
     /**
@@ -23,6 +26,30 @@ public class GameOperations {
                 button.setBounds(20 + j * 18, 60 + i * 18, 18, 18);
                 buttons[i][j] = button;
                 totalButtons--;
+            }
+        }
+    }
+
+    /**
+     * Set the number of surrounding mines for each button and add them to the JFrame.
+     * @param jFrame The JFrame to which the buttons are added.
+     * @param buttons The buttons that are available to the user.
+     */
+    public static void setSurroundingMines(JFrame jFrame, MineSweeperButton [][] buttons) {
+        for (int i = 0; i < Constants.ROWS; i++) {
+            for (int j = 0; j < Constants.COLS; j++) {
+                buttons[i][j].setNeighbors(new ArrayList<>());
+                for (Direction direction : Constants.ADJACENT_OFFSETS) {
+                    int newX = i + direction.getX();
+                    int newY = j + direction.getY();
+                    if (newX >= 0 && newX < Constants.ROWS && newY >= 0 && newY < Constants.COLS) {
+                        buttons[i][j].getNeighbors().add(buttons[newX][newY]);
+                        if (buttons[newX][newY].isMine()) {
+                            buttons[i][j].setNumberOfSurroundingMines(buttons[i][j].getNumberOfSurroundingMines() + 1);
+                        }
+                    }
+                }
+                jFrame.add(buttons[i][j]);
             }
         }
     }
--- src/main/java/org/gaming/Main.java
+++ src/main/java/org/gaming/Main.java
@@ -2,7 +2,6 @@ package org.gaming;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Main {
 
@@ -22,23 +21,7 @@ public class Main {
 
         MineSweeperButton [][] buttons = new MineSweeperButton[Constants.ROWS][Constants.COLS];
         GameOperations.setNewGame(buttons);
-
-        for (int i = 0; i < Constants.ROWS; i++) {
-            for (int j = 0; j < Constants.COLS; j++) {
-                buttons[i][j].setNeighbors(new ArrayList<>());
-                for (Direction direction : Constants.ADJACENT_OFFSETS) {
-                    int newX = i + direction.getX();
-                    int newY = j + direction.getY();
-                    if (newX >= 0 && newX < Constants.ROWS && newY >= 0 && newY < Constants.COLS) {
-                        buttons[i][j].getNeighbors().add(buttons[newX][newY]);
-                        if (buttons[newX][newY].isMine()) {
-                            buttons[i][j].setNumberOfSurroundingMines(buttons[i][j].getNumberOfSurroundingMines() + 1);
-                        }
-                    }
-                }
-                jFrame.add(buttons[i][j]);
-            }
-        }
+        GameOperations.setSurroundingMines(jFrame, buttons);
 
         resetButton.addActionListener(event -> {
             jFrame.dispose();
